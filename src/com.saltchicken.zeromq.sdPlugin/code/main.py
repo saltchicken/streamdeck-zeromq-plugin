4f65cbfc7d4ab6081e5579e37b17d3019c754ffc
saltchicken <john.eicher89@gmail.com>
2023-09-10 16:35:35 -0700

    Implement savefile Action.

--- src/com.saltchicken.zeromq.sdPlugin/code/main.py
+++ src/com.saltchicken.zeromq.sdPlugin/code/main.py
@@ -26,7 +26,18 @@ class ShowImage(Action):
         # union_type = obj.payload.settings.get("union_type", "or")
         # grayscale_flag = obj.payload.settings.get("grayscale_flag", False)
         
-        self.socket.send_string(category)
+        self.socket.send_string("ShowImage")
+        
+class SaveImage(Action):
+    def __init__(self, socket):
+        self.socket = socket
+    UUID = "com.saltchicken.zeromq.saveimage"
+    
+    def on_dial_rotate(self, obj: events_received_objs.DialRotate):
+        logger.info(obj.payload)
+
+    def on_key_down(self, obj: events_received_objs.KeyDown):        
+        self.socket.send_string("SaveImage")
         
 
 if __name__ == '__main__':
@@ -35,7 +46,8 @@ if __name__ == '__main__':
     socket.bind("tcp://*:5557")
     StreamDeck(
         actions=[
-            ShowImage(socket)
+            ShowImage(socket),
+            SaveImage(socket)
         ],
         log_file=settings.LOG_FILE_PATH,
         log_level=settings.LOG_LEVEL,
